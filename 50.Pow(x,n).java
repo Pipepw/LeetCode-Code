a3d861955a48a8339436141e4a0e767a19c2b613
Pipepw <416361536@qq.com>
2021-02-02 16:09:17 +0800

    perf(傻瓜版通过测试：98.25，58.96): 傻瓜版通过测试：98.25，58
    只是简单的新增了判断条件，并没有对算法有很多的改进，实际上可以改进为二进制形式，将为1的部分保留下来

--- 50.Pow(x,n).java
+++ 50.Pow(x,n).java
@@ -2,17 +2,21 @@ class Solution {
 
     public double myPow(double x, int n) {
         double res = 1;
-        if (n == 0) {
+        int flag = (n > 0) ? 1 : -1;
+        if (n == 0 || x == 1) {
             return res;
-        } else if (n > 0) {
-            for (int i = 0; i < n; i++) {
-                res *= x;
-            }
-        } else {
-            n *= -1;
-            for (int i = 0; i < n; i++) {
-                res *= x;
-            }
+        }
+        if (x == -1) {
+            return (n % 2 == 0) ? 1 : -1;
+        }
+        n = (n > 0) ? n : (n > -2147483648) ? -1 * n : -1;
+        if (n == -1) {
+            return 0;
+        }
+        for (int i = 0; i < n; i++) {
+            res *= x;
+        }
+        if (flag < 0) {
             res = 1 / res;
         }
         return res;
