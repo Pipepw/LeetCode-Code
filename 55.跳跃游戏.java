23ab0cd93d5ef0abce1b97480fda19fa3bd576b8
Pipepw <416361536@qq.com>
2021-02-05 11:20:50 +0800

    improvement(贪心算法): 81，24：只需要一次遍历就行了

--- 55.跳跃游戏.java
+++ 55.跳跃游戏.java
@@ -1,27 +1,16 @@
 class Solution {
-    int[] isReachable;
 
     public boolean canJump(int[] nums) {
-        // 默认就是false，这样没办法记录新的结果，所以用int
-        isReachable = new int[nums.length]; // 默认为0，成功为1，失败为2
-        find(nums, 0);
-        return isReachable[0] == 1;
-    }
-
-    public void find(int[] nums, int loc) {
-        if (loc + nums[loc] >= nums.length - 1) {
-            isReachable[loc] = 1;
-            return;
-        }
-        for (int i = 1; i <= nums[loc]; i++) {
-            if (isReachable[loc + i] == 0) {
-                find(nums, loc + i);
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > max) {
+                return false;
             }
-            if (isReachable[loc + i] == 1) {
-                isReachable[loc] = 1;
-                return;
+            if (max > nums.length - 1) {
+                return true;
             }
+            max = Math.max(max, i + nums[i]);
         }
-        isReachable[loc] = 2;
+        return true;
     }
 }
