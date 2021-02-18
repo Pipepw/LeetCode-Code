b5711d8d9ea5545f84521618524120b28e6bdcd0
Pipepw <416361536@qq.com>
2021-02-18 09:34:05 +0800

    feat(剪枝后的动态规划): 100，89；左和上之间有一个重叠的部分，我进行了重复计算，所以超时，所以用数组保存下来

--- 62.不同路径.java
+++ 62.不同路径.java
@@ -1,10 +1,20 @@
 class Solution {
+    int[][] nums;
+
     public int uniquePaths(int m, int n) {
-        if(m == 1 || n == 1){
+        nums = new int[m][n];
+        return findPaths(m, n);
+    }
+
+    public int findPaths(int m, int n) {
+        if (m == 1 || n == 1) {
             return 1;
         }
-        int left = uniquePaths(m, n-1);
-        int right = uniquePaths(m-1, n);
-        return left + right;
+        if (nums[m - 1][n - 1] == 0) {
+            int left = findPaths(m, n - 1);
+            int right = findPaths(m - 1, n);
+            nums[m - 1][n - 1] = left + right;
+        }
+        return nums[m - 1][n - 1];
     }
 }
